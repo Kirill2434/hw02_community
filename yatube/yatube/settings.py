d1d334be0e4bdff10061d18c7182b51bf7794b28
grish <cool.yn20@yandex.ru>
2022-03-30 20:46:15 +0300

    Создаю регистрацию_3

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +116,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 POSTS_PER_PAGE = 10
+
+LOGIN_URL = 'users:login'
+
+LOGIN_REDIRECT_URL = 'posts:index'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
