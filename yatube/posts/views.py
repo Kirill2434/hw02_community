8c24f5491247a66201eaad15e1c33205d3db0513
grish <cool.yn20@yandex.ru>
2022-04-05 20:03:12 +0300

    Разработал новые страницы с постами

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,7 @@
-from django.conf import settings
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator
 
-from .models import Group, Post
+from .models import Group, Post, User
 
 PAGE_NUM = 10
 
@@ -31,3 +30,29 @@ def group_posts(request, slug):
         'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = User.objects.get(username=username)
+    post_list = Post.objects.filter(author=author)
+    posts_number = post_list.count()
+    paginator = Paginator(post_list, PAGE_NUM)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'author': author,
+        'posts_number': posts_number,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    posts = Post.objects.get(id=post_id)
+    posts_numbers = Post.objects.count()
+    context = {
+        'posts': posts,
+        'posts_numbers': posts_numbers,
+    }
+    return render(request, 'posts/post_detail.html', context)
+
